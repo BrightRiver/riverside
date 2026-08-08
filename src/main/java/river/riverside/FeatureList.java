ab3c3049b3dd031be7b9b175e22b45ea2e5671db
brightriver <brightriver.mastermind@gmail.com>
2026-08-08 15:03:17 +0100

    added wooden walls with recipes

--- src/main/java/river/riverside/FeatureList.java
+++ src/main/java/river/riverside/FeatureList.java
@@ -5,10 +5,10 @@ import river.riverside.core.providers.PackProviders;
 public class FeatureList {
 
     public static void initializeFeatures() {
-        //add feature registration calls here
+        WoodWalls.initialize();
     }
 
     public static void generateData(PackProviders providers) {
-        //add feature data generation calls here
+        new WoodWallsData(providers);
     }
 }
